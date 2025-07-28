49bca9eaa6b0e23e73c3c09c60450e4b3cf987f5
LucRETius Dev <jraskin3@jolteon.cs.binghamton.edu>
2025-07-28 13:43:21 -0400

    fixing up acc plot

--- scripts/graphs/accuracy_plot.py
+++ scripts/graphs/accuracy_plot.py
@@ -13,9 +13,11 @@ parser.set_defaults(verbose=False)
 args = parser.parse_args() 
 ratio_data = args.ratio_data
 
-ratio_mean_df = pd.read_csv(ratio_data)
-ratio_mean_df = ratio_mean_df.sort_index()
-ratio_mean_df = ratio_mean_df.reset_index(names=["benchmark"])
+ratio_mean_df = pd.read_csv(ratio_data).fillna(0)
+ratio_mean_df = ratio_mean_df.sort_values("benchmark")
+
+std_val = ratio_mean_df['mean'].std()
+std_val = 0 if pd.isna(std_val) else std_val
 
 mpl.rcParams['axes.labelsize'] = "x-large"
 mpl.rcParams['axes.titlesize'] = 'x-large'
@@ -25,7 +27,7 @@ fig = plt.gcf()
 fig, ax = plt.subplots(figsize=(10, 3))
 ax.bar(ratio_mean_df.index, ratio_mean_df["mean"]*100, yerr=ratio_mean_df["std"]*100, capsize=2, color="tab:purple", edgecolor="black")
 print(f"\nAvg Error = {(ratio_mean_df['mean'].mean() * 100):.2f}%\nMedian Error = {(ratio_mean_df['mean'].median() * 100):.2f}%")
-plt.ylim(0,100 * (ratio_mean_df['mean'].max() + 2 * ratio_mean_df['mean'].std()))
+plt.ylim(0,100 * (ratio_mean_df['mean'].max() + 2 * std_val))
 plt.xlabel("Benchmark")
 plt.ylabel("Percent Error")
 fmt = '%.0f%%' # Format you want the ticks, e.g. '40%'
